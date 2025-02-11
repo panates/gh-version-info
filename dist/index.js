0542cc7f8366a3b7b8d9fc7d0567ba0a3004ccf1
Eray Hanoğlu <e.hanoglu@panates.com>
2025-02-11 13:18:59 +0300

    dev: Dev changes

--- dist/index.js
+++ dist/index.js
@@ -32299,7 +32299,6 @@ const token = core.getInput('token') || process.env.GITHUB_TOKEN || '';
 const octokit = github.getOctokit(token);
 const VERSION_PATTERN = /[Vv](\d)+\.(\d)+\.(\d)+/;
 const run = async () => {
-    console.log(github.context);
     const tagsRequest = await octokit.rest.repos.listTags({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
@@ -32308,13 +32307,14 @@ const run = async () => {
         .filter(t => VERSION_PATTERN.test(t.name))
         .map(t => t.name)
         .sort((x, y) => x.localeCompare(y));
-    console.log(`List of version tags are ${versions}`);
+    // console.log(`List of version tags are ${versions.join('\n  ')}`);
+    console.log(`List of version tags are ${JSON.stringify(tagsRequest.data, null, 2)}`);
     const releasesRequest = await octokit.rest.repos.listReleases({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
     });
     const releases = releasesRequest.data;
-    console.log(`List of releases are ${releases}`);
+    console.log(`List of releases are ${JSON.stringify(releases, null, 2)}`);
     // const nextVersionTag = getNextVersion(orderedTags[0]);
     // console.log(`next version is ${nextVersionTag}`);
     core.setOutput('versions', versions);
