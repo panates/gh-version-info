01fd8c6d0a2c80da57e240913749c36098b65b23
Eray Hanoğlu <e.hanoglu@panates.com>
2025-02-11 13:30:29 +0300

    dev: Dev changes

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,57 @@ const octokit = github.getOctokit(token);
 const VERSION_PATTERN = /[Vv](\d)+\.(\d)+\.(\d)+/;
 
 const run = async () => {
+  const releasedOnly = core.getInput('released-only') === 'true';
+
   const tagsRequest = await octokit.rest.repos.listTags({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  });
+  const releasesRequest = await octokit.rest.repos.listReleases({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
   });
 
   const versions = tagsRequest.data
     .filter(t => VERSION_PATTERN.test(t.name))
-    .map(t => t.name)
-    .sort((x, y) => x.localeCompare(y));
+    .map(t => {
+      const o: any = {
+        name: t.name,
+        commit: t.commit.sha,
+        released: false,
+        release: null,
+      };
+      const release = releasesRequest.data.find(r => r.tag_name === t.name);
+      if (release) {
+        o.released = true;
+        o.release = {
+          id: release.id,
+          url: release.url,
+          draft: release.draft,
+          prerelease: release.prerelease,
+          published_at: release.published_at,
+          notes: release.body,
+        };
+      }
+      return o;
+    })
+    .filter(t => !releasedOnly || t.released)
+    .sort((x, y) => x.name.localeCompare(y.name));
+
+  const releases = releasesRequest.data;
 
   // console.log(`List of version tags are ${versions.join('\n  ')}`);
   console.log(
     `List of version tags are ${JSON.stringify(tagsRequest.data, null, 2)}`,
   );
-
-  const releasesRequest = await octokit.rest.repos.listReleases({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-  });
-
-  const releases = releasesRequest.data;
-
   console.log(`List of releases are ${JSON.stringify(releases, null, 2)}`);
 
   // const nextVersionTag = getNextVersion(orderedTags[0]);
   // console.log(`next version is ${nextVersionTag}`);
 
-  core.setOutput('versions', versions);
-  core.setOutput('releases', releases);
+  core.setOutput('count', versions.length);
+  core.setOutput('items', versions);
+  // core.setOutput('releases', releases);
   // core.setOutput('next', nextVersionTag);
 };
 
